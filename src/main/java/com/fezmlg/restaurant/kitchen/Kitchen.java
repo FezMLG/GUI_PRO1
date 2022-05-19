1ee9bf86c3eb4fddfcd0ff767d9fbc11d9e0d73c
Konrad Chwesiuk <46359181+FezMLG@users.noreply.github.com>
2022-05-19 23:12:23 +0200

    change: add thread example

--- src/main/java/com/fezmlg/restaurant/kitchen/Kitchen.java
+++ src/main/java/com/fezmlg/restaurant/kitchen/Kitchen.java
@@ -1,4 +1,19 @@
 package com.fezmlg.restaurant.kitchen;
 
-public class Kitchen {
+import java.util.concurrent.TimeUnit;
+
+public class Kitchen implements Runnable {
+    public static void main(String[] args) {
+        Kitchen kitchen = new Kitchen();
+        Thread thread = new Thread(kitchen);
+        thread.start();
+        while (thread.isAlive()) {
+            System.out.println("Waiting...");
+        }
+        System.out.println("This code is outside of the thread");
+    }
+
+    public void run() {
+        System.out.println("This code is running in a thread");
+    }
 }
